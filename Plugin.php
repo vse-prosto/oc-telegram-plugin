50ae45478a44bf3e183fea9f29e5979b76a128db
vse-prosto <info@all-simple.ru>
2021-12-18 12:48:15 +0300

    first commit

---
+++ Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace VseProsto\Telegram;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Telegram Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Telegram',
+            'description' => 'No description provided yet...',
+            'author'      => 'VseProsto',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'VseProsto\Telegram\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'vseprosto.telegram.some_permission' => [
+                'tab' => 'Telegram',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'telegram' => [
+                'label'       => 'Telegram',
+                'url'         => Backend::url('vseprosto/telegram/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['vseprosto.telegram.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
